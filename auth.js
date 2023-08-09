624e1aa776a18bfa6d2a88d084c0d58c7c3b8017
Salmansha08 <salmanwiharja8@gmail.com>
2023-08-09 11:58:32 +0700

    Add Github OAuth tapi masih pakai Passport.JS

--- auth.js
+++ auth.js
@@ -1,7 +1,9 @@
 const passport  = require('passport');
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+const GitHubStrategy = require('passport-github2').Strategy;
 require('dotenv').config();
 
+// GOOGLE
 passport.use(
   new GoogleStrategy(
     {
@@ -12,9 +14,35 @@ passport.use(
     },
     function(request, accessToken, refreshToken, profile, done) {
       done(null, profile);
+    },
+    /*
+    function(request, accessToken, refreshToken, profile, done) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return done(err, user);
+      });
     }
+    */
   )
 );
+
+//GITHUB
+passport.use(
+  new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:5000/auth/github/callback"
+  },
+  function(request, accessToken, refreshToken, profile, done) {
+    done(null, profile);
+  },
+  /*
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+  */
+));
 
 passport.serializeUser((user, done) => {
     done(null, user);
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const passport = require('passport');
+
+const axios = require('axios');
+
+
 const path = require('path');
 const app = express();
 require('dotenv').config();
@@ -10,10 +14,8 @@ require('./auth');
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "client")));
 
-function isLoggedIn(req, res, next) {
-    req.user ? next() : res.sendStatus(401);
-}
 
+// MAIN PAGE
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
@@ -30,6 +32,11 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+//GOOGLE
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
+
 app.get(
     '/auth/google',
     passport.authenticate('google', { 
@@ -39,24 +46,52 @@ app.get(
 
 app.get('/auth/google/callback',
     passport.authenticate('google', {
-        successRedirect: '/auth/protected',
+        successRedirect: '/auth/google/success',
         failureRedirect: '/auth/google/failure'
     })
 );
 
-
-app.get('/auth/google/failure', (req, res) => {
-    res.send('Something went wrong!');
-});
-
-app.get('/auth/protected', isLoggedIn, (req, res) => {
+app.get('/auth/google/success', isLoggedIn, (req, res) => {
     let name = req.user.displayName;
     res.send(`Hello ${name}`);
 });
 
-app.use('/auth/logout', (req, res) => {
+app.get('/auth/google/failure', (req, res) => {
+    res.send('Gagal Login Google!');
+});
+
+app.use('/auth/google/logout', (req, res) => {
     req.session.destroy();
-    res.send('See you again!');
+    res.send('Berhasil Logout Google!');
+});
+
+//GITHUB
+app.get(
+    '/auth/github',
+    passport.authenticate('github', { 
+        scope: [ 'user:email' ]
+    })
+);
+
+app.get('/auth/github/callback', 
+  passport.authenticate('github', {
+    successRedirect: '/auth/github/success', 
+    failureRedirect: '/auth/github/failure' }),
+ 
+);
+
+app.get('/auth/github/success', isLoggedIn, (req, res) => {
+    let name = req.user.displayName;
+    res.send(`Hello ${name}`);
+});
+
+app.get('/auth/github/failure', (req, res) => {
+    res.send('Gagal Login Github!');
+});
+
+app.use('/auth/google/logout', (req, res) => {
+    req.session.destroy();
+    res.send('Berhasil Logout Github!');
 });
 
 app.listen(5000, () => {
